b6eed6bffc52ad3169693687fd57d7b7ef6c09b0
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-04-20 17:42:49 +0200

    make five kodilla tasks

--- script-compiled.js
+++ script-compiled.js
@@ -1,1 +1,43 @@
-"use strict";
+'use strict';
+
+// Pierwsze zadanie
+
+var firstWord = ['Hello'];
+var secondWord = ['World'];
+var arrayOfWords = [].concat(firstWord, secondWord);
+
+arrayOfWords.forEach(function (elem) {
+  return console.log(elem);
+});
+
+// Drugie zadanie
+
+var multiply = function multiply(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+
+// Trzecie zadanie
+
+var average = function average() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return args.reduce(function (prev, curr) {
+    return prev + curr;
+  }) / args.length;
+};
+
+// Czwarte zadanie
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+// Piąte zadanie 
+
+var weirdArray = [1, 4, 'Iwona', false, 'Nowak'];
+var firstName = weirdArray[2],
+    lastName = weirdArray[4];
+
+
+console.log(firstName, lastName);
--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+// Pierwsze zadanie
 
+const firstWord = [`Hello`];
+const secondWord = [`World`];
+const arrayOfWords = [...firstWord, ...secondWord];
 
+arrayOfWords.forEach(elem => console.log(elem));
+
+// Drugie zadanie
+
+const multiply = (a, b = 1) => a * b;
+
+// Trzecie zadanie
+
+let average = (...args) => args.reduce((prev, curr) => prev + curr) / args.length;
+
+// Czwarte zadanie
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+// Piąte zadanie 
+
+const weirdArray = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName, , lastName] = weirdArray;
+
+console.log(firstName, lastName);
